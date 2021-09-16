dc13248ed886042c46ce53fc24d64ff21423c6e5
Mahesh Deshwal <m.deshwal93@gmail.com>
2021-09-16 23:18:43 +0530

    NSE Live Data Update for indices for Intraday stock selection

--- helpers/intraday.py
+++ helpers/intraday.py
@@ -10,15 +10,18 @@ class IntraDay():
     Class for Intraday Screening of stocks
     '''
     
-    def intraday_whole_num_strategy(self,nifty=50):
+    def intraday_whole_num_strategy(self,nifty:int=50, min_val:int=100, max_val:int=5000):
         '''
         If Open == High or Low for a stock around 9:30, go long if Open == Low else go high only after 9:30 on 15 minutes candle
         args:
             nifty: Index to choose from File
+            min_val: Minimum value to consider
+            max_val: Maximum value of stock price to consider
         returns:
             Dictonary of tuples {"Long":[(name, Open, nifty Index), ...], "Short":[(name, Open, nifty Index), ...]}
         '''
-        df = pd.read_csv(join(expanduser('~'),'Downloads',f"MW-NIFTY-{nifty}-{present.day}-{calendar.month_abbr[present.month]}-{present.year}.csv"))
+        file = "MW-NIFTY500-MULTICAP-50_25_25" if nifty == 500 else f"MW-NIFTY-{nifty}"
+        df = pd.read_csv(join(expanduser('~'),'Downloads',f"{file}-{present.day}-{calendar.month_abbr[present.month]}-{present.year}.csv"))
 
         df['Equal'] = df.apply(lambda row: True if (row['OPEN \n'] == row['HIGH \n'] or row['OPEN \n'] == row['LOW \n']) else False, axis=1)
         df = df.loc[df["Equal"] == True,:]
@@ -28,12 +31,14 @@ class IntraDay():
         result = {'Long':[], 'Short':[]}
         for index in df.index:
             open_ = float(df.loc[index,'OPEN \n'].replace(',',''))
-            high =  float(df.loc[index,'HIGH \n'].replace(',',''))
-            low =  float(df.loc[index,'LOW \n'].replace(',',''))
-            ltp = float(df.loc[index,'LTP \n'].replace(',',''))
-            name = df.loc[index,'SYMBOL \n']
+            
+            if min_val < open_ < max_val: # if open is Greater than Max or less than Minimum value then don't consider
+                high =  float(df.loc[index,'HIGH \n'].replace(',',''))
+                low =  float(df.loc[index,'LOW \n'].replace(',',''))
+                ltp = float(df.loc[index,'LTP \n'].replace(',',''))
+                name = df.loc[index,'SYMBOL \n']
 
-            result['Long'].append((name,open_,In.get_index(name))) if open_ == low else result['Short'].append((name,open_,In.get_index(name)))
+                result['Long'].append((name,open_,In.get_index(name))) if open_ == low else result['Short'].append((name,open_,In.get_index(name)))
 
         return result
 
